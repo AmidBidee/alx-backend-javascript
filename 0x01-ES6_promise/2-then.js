02e8ae7c08dccc9da43a0bcf2049db81cfa3c602
krummitz <amidbidee@gmail.com>
2022-04-10 19:54:26 +0100

    eslint validated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-    const data = { status: 200, body: 'success' };
+  const data = { status: 200, body: 'success' };
 
-    return promise
-        .then(() => data)
-        .catch(() => new Error())
-        .finally(() => {
-            console.log('Got a response from the API');
-        });
+  return promise
+    .then(() => data)
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
