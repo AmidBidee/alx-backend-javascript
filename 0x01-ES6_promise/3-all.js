22d0d2531d4e9114f6a7e9d246a3440f7a3bfe1d
krummitz <amidbidee@gmail.com>
2022-04-13 00:57:34 +0100

    updates completed data manipulation tasks

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,7 @@
-import { uploadPhoto, createUser } from 'util';
+import {uploadPhoto, createUser} from 'util';
 
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-
-  return Promise.all([photo, user]).then(
-    (result) => console.log(`${result[0].body} ${result[1].firstname} ${result[1].lastname}`),
-    () => console.log('Signup system offline'),
-  );
+  
 }
